a70b43f170bfd49989a7cb3423d00a0d48ba55af
saurabh eknath dokade <saurabhdokade77@gmail.com>
2025-05-21 20:09:32 +0530

    .

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-  origin: ["http://localhost:3000","https://dashlite-nu.vercel.app/"],
+  origin: ["http://localhost:3000","https://dashlite-nu.vercel.app"],
   credentials: true
 }));
 
